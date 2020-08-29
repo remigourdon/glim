95d948399b354bf05ed797629010864b2bafd3a8
remigourdon <gourdon.remi@gmail.com>
2020-08-29 18:07:11 -0400

    Parse basic configuration file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
 use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg};
+use serde::Deserialize;
+use std::collections::HashMap;
+use std::path::PathBuf;
+
+#[derive(Deserialize, Debug)]
+struct Config {
+    repositories: HashMap<String, PathBuf>,
+}
 
 fn main() {
     let matches = App::new(crate_name!())
@@ -16,5 +24,7 @@ fn main() {
         .get_matches();
 
     let config = matches.value_of("config").unwrap_or("default.conf");
-    println!("{}", &config);
+    let config = std::fs::read_to_string(&config).unwrap();
+    let config: Config = toml::from_str(&config).unwrap();
+    println!("{:?}", config);
 }
