0e0fd5f1ab2675683c402b31880cd70f1b9f7b51
remigourdon <gourdon.remi@gmail.com>
2020-08-29 17:48:55 -0400

    Initial commit with basic clap configuration

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg};
+
+fn main() {
+    let matches = App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!("\n"))
+        .about(crate_description!())
+        .arg(
+            Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .value_name("FILE")
+                .help("Sets a custom config file")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let config = matches.value_of("config").unwrap_or("default.conf");
+    println!("{}", &config);
+}
