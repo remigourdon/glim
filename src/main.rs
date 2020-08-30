7e697d2e8cccba8ea08cb6cc1278aca5361f3e51
remigourdon <gourdon.remi@gmail.com>
2020-08-30 18:31:12 -0400

    Simplify config initialization

--- src/main.rs
+++ src/main.rs
@@ -67,20 +67,22 @@ fn main() {
         )
         .get_matches();
 
+    // Create config directory if it doesn't exist
+    let config_dir = project_dirs.config_dir();
+    if !config_dir.is_dir() {
+        std::fs::create_dir(config_dir).unwrap();
+    }
+
     // Load config from file or create new one
     let config_path = Path::new(matches.value_of("config").unwrap());
     let mut config: Config = if config_path.is_file() {
         Config::from_path(config_path)
-    } else if config_path == default_config_path {
-        let config_dir = project_dirs.config_dir();
-        // Create the app's config directory if it doesn't exist
-        if !config_dir.is_dir() {
-            std::fs::create_dir(config_dir).unwrap();
-        }
-        Config::new()
     } else {
-        panic!("Invalid config file");
+        Config::new()
     };
+
+    // Save config
+    config.save(config_path);
 
     if let Some(matches) = matches.subcommand_matches("add") {
         let mut added = 0;
