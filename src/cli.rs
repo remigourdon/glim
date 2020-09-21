f0557651cc4f37739fc1964b97aaccef6a5dd74e
Rémi Gourdon <gourdon.remi@gmail.com>
2020-09-20 20:29:47 -0400

    Use structopt on top of clap for CLI (#3)
    * Replace clap with structopt CLI
    
    * Add doc comments
    
    * Use default value for config
    
    * Rename option arguments and add descriptions

---
+++ src/cli.rs
@@ -0,0 +1,219 @@
+use crate::config::Config;
+use crate::repository::Repository;
+
+use std::collections::BTreeMap;
+use std::path::PathBuf;
+use std::sync::mpsc::channel;
+
+use anyhow::{Context, Result};
+use indicatif::{ProgressBar, ProgressStyle};
+use prettytable::{cell, format, row, Table};
+use structopt::StructOpt;
+use threadpool::ThreadPool;
+
+#[derive(StructOpt)]
+#[structopt(about)]
+pub struct CLI {
+    /// Set a custom config file
+    #[structopt(value_name = "FILE", short, long, default_value)]
+    config: Config,
+
+    /// Do not fetch
+    #[structopt(short = "F", long)]
+    no_fetch: bool,
+
+    /// Number of workers
+    #[structopt(value_name = "NUM_WORKERS", short, long, default_value = "4")]
+    workers: usize,
+
+    #[structopt(subcommand)]
+    command: Option<Command>,
+}
+
+#[derive(StructOpt)]
+enum Command {
+    /// Add new repositories
+    Add {
+        /// Paths to the repositories to add
+        #[structopt(value_name = "PATH")]
+        path: Vec<PathBuf>,
+    },
+    /// Remove repositories
+    Remove {
+        /// Names of the repositories to remove
+        #[structopt(value_name = "NAME")]
+        name: Vec<String>,
+    },
+    /// Rename repository
+    Rename {
+        /// Current name of the repository
+        #[structopt(value_name = "NAME")]
+        name: String,
+        /// New name of the repository
+        #[structopt(value_name = "NEW_NAME")]
+        new_name: String,
+    },
+    /// Get repository's path
+    Path {
+        /// Name of the repository
+        #[structopt(value_name = "NAME")]
+        name: String,
+    },
+}
+
+impl CLI {
+    pub fn run(&mut self) -> Result<()> {
+        let ran_command = self.run_command()?;
+        // Process repositories and display only if a subcommand wasn't run
+        if !ran_command {
+            self.process_and_display()
+        } else {
+            Ok(())
+        }
+    }
+    fn run_command(&mut self) -> Result<bool> {
+        let mut ran_command = true;
+        let mut modified = false;
+        match &self.command {
+            Some(Command::Add { path }) => {
+                for path in path {
+                    self.config.add_repository(path)?;
+                    modified = true;
+                }
+            }
+            Some(Command::Remove { name }) => {
+                for name in name {
+                    if self.config.remove_repository_by_name(&name) {
+                        modified = true;
+                    }
+                }
+            }
+            Some(Command::Rename { name, new_name }) => {
+                self.config.rename_repository(&name, &new_name)?;
+                modified = true;
+            }
+            Some(Command::Path { name }) => {
+                let path = self
+                    .config
+                    .repositories()
+                    .get(name)
+                    .context("name does not exist")?;
+                println!("{:?}", path);
+            }
+            None => {
+                ran_command = false;
+            }
+        }
+
+        // Save config
+        if modified {
+            self.config.save().context("failed to save config")?;
+        }
+        Ok(ran_command)
+    }
+    fn process_and_display(&self) -> Result<()> {
+        // Attempt to open repositories
+        let mut repositories = Vec::with_capacity(self.config.repositories().len());
+        for (name, path) in self.config.repositories() {
+            match Repository::open(name, path) {
+                Ok(repository) => repositories.push(repository),
+                Err(e) => eprintln!("Could not open '{}': {}", name, e),
+            }
+        }
+
+        // Create thread pool
+        let pool = ThreadPool::new(self.workers);
+        let (tx, rx) = channel();
+        let num_jobs = repositories.len();
+
+        // Create progress bar
+        let pb = ProgressBar::new(num_jobs as u64);
+        pb.set_style(
+            ProgressStyle::default_bar()
+                .template("{prefix} [{bar:60}] {pos}/{len}: {msg}")
+                .progress_chars("=> "),
+        );
+        pb.set_prefix("Processing...");
+
+        let do_fetch = !self.no_fetch;
+
+        // Process repositories on thread pool
+        for repository in repositories.into_iter() {
+            let mut repository = repository;
+            let tx = tx.clone();
+            let pb = pb.clone();
+
+            pool.execute(move || {
+                // Attempt to fetch from repository
+                if do_fetch {
+                    let _ = repository.fetch();
+                }
+                // Compute status now since it can be slow
+                let _ = repository.compute_status();
+
+                // Update progress bar
+                pb.set_message(repository.name());
+                pb.inc(1);
+
+                tx.send(repository).unwrap();
+            });
+        }
+
+        // Join threads and collect data in a sorted map
+        let sorted_map = rx
+            .iter()
+            .take(num_jobs)
+            .fold(BTreeMap::new(), |mut map, repository| {
+                map.insert(repository.name().to_string(), repository);
+                map
+            });
+
+        // Clear progress bar
+        pb.finish_and_clear();
+
+        // Create table
+        let mut table = Table::new();
+
+        // Format table
+        let format = format::FormatBuilder::new()
+            .column_separator(' ')
+            .borders(' ')
+            .padding(0, 3)
+            .build();
+        table.set_format(format);
+
+        // Add rows to table
+        for (name, repository) in sorted_map.iter() {
+            // Get status
+            let status = if let Some(status) = repository.status() {
+                status.to_string()
+            } else {
+                String::new()
+            };
+            // Get distance between local and upstream
+            let distance = if let Some(distance) = repository.distance() {
+                distance.to_string()
+            } else {
+                String::new()
+            };
+            table.add_row(row![
+                name,
+                status,
+                repository.branch_name().unwrap_or_default().to_string(),
+                distance,
+                repository.remote_name().unwrap_or_default().to_string(),
+                repository
+                    .commit_summary()
+                    .unwrap_or_default()
+                    .chars()
+                    .take(50)
+                    .collect::<String>()
+            ]);
+        }
+
+        // Display table
+        table.printstd();
+
+        Ok(())
+    }
+}
--- src/config.rs
+++ src/config.rs
@@ -1,22 +1,26 @@
-use anyhow::{anyhow, Result};
-use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::fmt;
 use std::path::{Path, PathBuf};
+use std::str::FromStr;
+
+use anyhow::{anyhow, Context, Result};
+use directories::ProjectDirs;
+use serde::{Deserialize, Serialize};
+use structopt::clap::crate_name;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Config {
+    #[serde(skip)]
+    path: PathBuf,
     repositories: HashMap<String, PathBuf>,
 }
 
 impl Config {
-    pub fn new() -> Self {
-        Self {
-            repositories: HashMap::new(),
-        }
-    }
     pub fn from_path<P: AsRef<Path>>(path: P) -> Result<Self> {
+        let path = path.as_ref();
         let string = std::fs::read_to_string(path)?;
-        let config: Config = toml::from_str(&string)?;
+        let mut config: Config = toml::from_str(&string)?;
+        config.path = path.to_owned();
         Ok(config)
     }
     pub fn repositories(&self) -> &HashMap<String, PathBuf> {
@@ -55,7 +59,44 @@ impl Config {
             Ok(())
         }
     }
-    pub fn save<P: AsRef<Path>>(&self, path: P) -> Result<()> {
-        Ok(std::fs::write(path.as_ref(), toml::to_vec(self)?)?)
+    pub fn save(&self) -> Result<()> {
+        let mut path = self.path.clone();
+        if path.pop() {
+            // Create config directory if it doesn't exist
+            if !path.is_dir() {
+                std::fs::create_dir(path).context("failed to create config directory")?;
+            }
+            Ok(std::fs::write(&self.path, toml::to_vec(self)?)?)
+        } else {
+            Err(anyhow!("invalid config directory path"))
+        }
+    }
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        // Get default config path
+        let app_name = crate_name!();
+        let project_dirs = ProjectDirs::from("com", app_name, app_name)
+            .expect("could not retrieve home directory from system");
+        let default_config_path = project_dirs.config_dir().join("config.toml");
+        Self {
+            path: default_config_path,
+            repositories: HashMap::new(),
+        }
+    }
+}
+
+impl FromStr for Config {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Self::from_path(s).map_err(|_| "Could not create config from path")
+    }
+}
+
+impl fmt::Display for Config {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.path.to_str().unwrap())
     }
 }
